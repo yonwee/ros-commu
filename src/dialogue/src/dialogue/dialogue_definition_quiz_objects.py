4245e46ee602d665164c053fcac90a18620e9b40
nemoninja <35476293+nemoninja@users.noreply.github.com>
2018-02-16 13:55:14 +0900

    hello world. they are so rude.

--- src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
+++ src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
@@ -20,57 +20,88 @@ class DialogueLibraryQuiz(DialogueLibrary):
         :return:        The Dialogue concerning the object.
         """
         #return Dialogue(DialogueLibraryQuiz.select_convo(self,topic))
-        return Dialogue(DialogueLibraryQuiz.dialogue_string_test(self))
+        #return Dialogue(DialogueLibraryQuiz.dialogue_string_test(self))
 
-        # return Dialogue(
-        #     DialogueActionLook(
-        #         look_type=DialogueActionLook.LOOK_TYPE_WATCH_CONVERSATION_PARTNER,
-        #         cancelable=False,
-        #         next_action=
-        #         DialogueActionSleep(
-        #             sleep_time=1,
-        #             cancelable=False,
-        #             next_action=
-        #             DialogueActionTalkBinaryResponse(
-        #                 utterance="Do you also see {}?".format(self.__add_a_to_noun(self.__get_object_noun(topic))),
-        #                 cancelable=False,
-        #                 next_action_yes=
-        #                 DialogueActionTalkNoResponse(
-        #                     utterance=random.choice(self.positive_response_list),
-        #                     cancelable=False,
-        #                     next_action=
-        #                     DialogueActionLook(
-        #                         look_type=DialogueActionLook.LOOK_TYPE_WATCH_ENVIRONMENT,
-        #                         cancelable=True,
-        #                         next_action=None
-        #                     )
-        #                 ),
-        #                 next_action_no=
-        #                 DialogueActionLook(
-        #                     look_type=DialogueActionLook.LOOK_TYPE_WATCH_CONVERSATION_TOPIC,
-        #                     cancelable=False,
-        #                     next_action=
-        #                     DialogueActionTalkNoResponse(
-        #                         utterance=random.choice(self.negative_response_list),
-        #                         cancelable=False,
-        #                         next_action=
-        #                         DialogueActionSleep(
-        #                             sleep_time=2,
-        #                             cancelable=False,
-        #                             next_action=DialogueActionLook(
-        #                                 look_type=DialogueActionLook.LOOK_TYPE_WATCH_ENVIRONMENT,
-        #                                 cancelable=True,
-        #                                 next_action=None
-        #                             )
-        #                         )
-        #                     ),
-        #                 )
-        #             )
-        #         )
-        #     )
-        # )
+        return Dialogue(
+            DialogueActionLook(
+                look_type=DialogueActionLook.LOOK_TYPE_WATCH_CONVERSATION_PARTNER,
+                cancelable=False,
+                next_action=
+                DialogueActionSleep(
+                    sleep_time=1,
+                    cancelable=False,
+                    next_action=
+                    DialogueActionTalkBinaryResponse(
+                        utterance="Do you also see {}?".format(self.__add_a_to_noun(self.__get_object_noun(topic))),
+                        cancelable=False,
+                        next_action_yes=
+                        DialogueActionTalkNoResponse(
+                            utterance=random.choice(self.positive_response_list),
+                            cancelable=False,
+                            next_action=
+                            DialogueActionLook(
+                                look_type=DialogueActionLook.LOOK_TYPE_WATCH_ENVIRONMENT,
+                                cancelable=True,
+                                next_action=None
+                            )
+                        ),
+                        next_action_no=
+                        DialogueActionLook(
+                            look_type=DialogueActionLook.LOOK_TYPE_WATCH_CONVERSATION_TOPIC,
+                            cancelable=False,
+                            next_action=
+                            DialogueActionTalkNoResponse(
+                                utterance=random.choice(self.negative_response_list),
+                                cancelable=False,
+                                next_action=
+                                DialogueActionSleep(
+                                    sleep_time=2,
+                                    cancelable=False,
+                                    next_action=DialogueActionLook(
+                                        look_type=DialogueActionLook.LOOK_TYPE_WATCH_ENVIRONMENT,
+                                        cancelable=True,
+                                        next_action=None
+                                    )
+                                )
+                            ),
+                        )
+                    )
+                )
+            )
+        )
+
+    def __add_a_to_noun(self, noun):
+        # type: (str) -> str
+
+        if noun[0].lower() in ['a', 'e', 'i', 'o', 'u']:
+            return 'an ' + noun
+        else:
+            return 'a ' + noun
+
+    def __get_object_noun(self, label):
+        return self.object_proper_name_map.get(label, label)
+
+    object_proper_name_map = {
+        'aeroplane': 'airplane',
+        'diningtable': 'dining table',
+        'pottedplant': 'potted plant',
+        'tvmonitor': 'screen'
+    }
+
+    positive_response_list = [
+        "Cool, you're good!",
+        "Nice, me too!",
+        "I see it too!"
+    ]
+
+    negative_response_list = [
+        "I see it over there",
+        "It's over there",
+        "I can help you. It's over here"
+    ]
 
 
+    """
     def __add_a_to_noun(self, noun):
         # type: (str) -> str
 
@@ -144,3 +175,4 @@ class DialogueLibraryQuiz(DialogueLibrary):
             return "None"+ ')'*(i-2)
 
         return fulldialogue
+    """

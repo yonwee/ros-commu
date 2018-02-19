5e03c4ee142f6eb8127e64d3f575addb7004da46
nemoninja <35476293+nemoninja@users.noreply.github.com>
2018-02-19 17:51:32 +0900

    Update dialogue_definition_quiz_objects.py

--- src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
+++ src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
@@ -29,7 +29,8 @@ class DialogueLibraryQuiz(DialogueLibrary):
                 utterance="Do you also see {}?".format(self.__add_a_to_noun(self.__get_object_noun(topic))),
                 cancelable=False,
                 next_action_yes=None))
-                    """DialogueActionLook(
+        """
+            DialogueActionLook(
                 look_type=DialogueActionLook.LOOK_TYPE_WATCH_CONVERSATION_PARTNER,
                 cancelable=False,
                 next_action=
@@ -79,7 +80,8 @@ class DialogueLibraryQuiz(DialogueLibrary):
                     )
                 )
             )
-        )        """
+        )        
+        """
             
     def __add_a_to_noun(self, noun):
         # type: (str) -> str

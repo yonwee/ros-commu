b8b244b42fe6712215b15799f3e6fee9733dae00
nemoninja <35476293+nemoninja@users.noreply.github.com>
2018-02-19 17:59:28 +0900

    Update dialogue_definition_quiz_objects.py

--- src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
+++ src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
@@ -7,6 +7,7 @@ from dialogue_manager import DialogueLibrary
 import urllib2
 import json
 
+
 class DialogueLibraryQuiz(DialogueLibrary):
     """
     A DialogueLibrary that can be used when a CommU robot sees an object. This plays 'object hide-and-seek' with the user.
@@ -18,71 +19,72 @@ class DialogueLibraryQuiz(DialogueLibrary):
         Get the dialogue that can be used when CommU sees an object.
         :param topic:   The label assigned by the ssd network
         :return:        The Dialogue concerning the object.
-        
+
         #return Dialogue(DialogueLibraryQuiz.select_convo(self,topic))
         #return Dialogue(self.dialogue_string_test())
         return Dialogue(DialogueActionTalkNoResponse(funman()))
-
         """
         return Dialogue(
             DialogueActionTalkNoResponse(
                 utterance="Do you also see {}?".format(self.__add_a_to_noun(self.__get_object_noun(topic))),
                 cancelable=False,
-                next_action_yes=None))
-"""
-            DialogueActionLook(
-                look_type=DialogueActionLook.LOOK_TYPE_WATCH_CONVERSATION_PARTNER,
+                next_action_yes=None
+            )
+        )
+    """
+    DialogueActionLook(
+        look_type=DialogueActionLook.LOOK_TYPE_WATCH_CONVERSATION_PARTNER,
+        cancelable=False,
+        next_action=
+        DialogueActionSleep(
+            sleep_time=1,
+            cancelable=False,
+            next_action=
+            DialogueActionTalkBinaryResponse(
+                utterance="Do you also see {}?".format(self.__add_a_to_noun(self.__get_object_noun(topic))),
                 cancelable=False,
-                next_action=
-                DialogueActionSleep(
-                    sleep_time=1,
+                next_action_yes=
+                DialogueActionTalkNoResponse(
+                    #utterance=random.choice(self.positive_response_list)
+                    #utterance=self.dialogue_string_test().format(self.__add_a_to_noun(self.__get_object_noun(topic))),
+                    #self.funman2(),
+                    #exec(self.dialogue_string_test()),
+                    #self.funman(),
+                    cancelable=False,
+                    #cancelable=self.dialogue_string_test(),
+                    next_action=
+                    DialogueActionLook(
+                        look_type=DialogueActionLook.LOOK_TYPE_WATCH_ENVIRONMENT,
+                        cancelable=True,
+                        next_action=None
+                    )
+                ),
+                next_action_no=
+                DialogueActionLook(
+                    look_type=DialogueActionLook.LOOK_TYPE_WATCH_CONVERSATION_TOPIC,
                     cancelable=False,
                     next_action=
-                    DialogueActionTalkBinaryResponse(
-                        utterance="Do you also see {}?".format(self.__add_a_to_noun(self.__get_object_noun(topic))),
+                    DialogueActionTalkNoResponse(
+                        utterance=random.choice(self.negative_response_list),
                         cancelable=False,
-                        next_action_yes=
-                        DialogueActionTalkNoResponse(
-                            #utterance=random.choice(self.positive_response_list)
-                            #utterance=self.dialogue_string_test().format(self.__add_a_to_noun(self.__get_object_noun(topic))),
-                            #self.funman2(),
-                            #exec(self.dialogue_string_test()),
-                            #self.funman(),
+                        next_action=
+                        DialogueActionSleep(
+                            sleep_time=2,
                             cancelable=False,
-                            #cancelable=self.dialogue_string_test(),
-                            next_action=
-                            DialogueActionLook(
+                            next_action=DialogueActionLook(
                                 look_type=DialogueActionLook.LOOK_TYPE_WATCH_ENVIRONMENT,
                                 cancelable=True,
                                 next_action=None
                             )
-                        ),
-                        next_action_no=
-                        DialogueActionLook(
-                            look_type=DialogueActionLook.LOOK_TYPE_WATCH_CONVERSATION_TOPIC,
-                            cancelable=False,
-                            next_action=
-                            DialogueActionTalkNoResponse(
-                                utterance=random.choice(self.negative_response_list),
-                                cancelable=False,
-                                next_action=
-                                DialogueActionSleep(
-                                    sleep_time=2,
-                                    cancelable=False,
-                                    next_action=DialogueActionLook(
-                                        look_type=DialogueActionLook.LOOK_TYPE_WATCH_ENVIRONMENT,
-                                        cancelable=True,
-                                        next_action=None
-                                    )
-                                )
-                            ),
                         )
-                    )
+                    ),
                 )
             )
-        )        
+        )
+    )
+)        
 """
-            
+
     def __add_a_to_noun(self, noun):
         # type: (str) -> str
 
@@ -114,75 +116,60 @@ class DialogueLibraryQuiz(DialogueLibrary):
     ]
 
 
-      
- """
-    def dialogue_string_test(self):
-        i=1
-        cjdata = self.request_script()
-        #dia = self.generation(cjdata,i)
-        keyvar = "1"
-        #dia = 'utterance=' + cjdata[keyvar]['1']
-        #dia = cjdata[keyvar]['1']
-        return cjdata
+"""
+   def dialogue_string_test(self):
+       i=1
+       cjdata = self.request_script()
+       #dia = self.generation(cjdata,i)
+       keyvar = "1"
+       #dia = 'utterance=' + cjdata[keyvar]['1']
+       #dia = cjdata[keyvar]['1']
+       return cjdata
+   def request_script(self):
+       convo = urllib2.urlopen('http://192.168.1.225:8080/?json={tast}')
+       cjson = convo.read()
+       cjdata = json.loads(cjson)
+       return cjdata
+
+   def funman(self):
+       cjdata = self.request_script()
+       keyvar = 1
+       while cjdata[keyvar]:
+       utterance = cjdata[keyvar]['1'].format(self.__add_a_to_noun(self.__get_object_noun(topic))
+       cancelable = cjdata[keyvar]['2']
+       next_action = cjdata[keyvar]['3']
+       keyvar += 1
+       return utterance, cancelable, next_action
 
 
-    def request_script(self):
-        convo = urllib2.urlopen('http://192.168.1.225:8080/?json={tast}')
-        cjson = convo.read()
-        cjdata = json.loads(cjson)
-        return cjdata
-    
-    def funman(self):
-        cjdata = self.request_script()
-        keyvar = 1
-        while cjdata[keyvar]:
-        utterance = cjdata[keyvar]['1'].format(self.__add_a_to_noun(self.__get_object_noun(topic))
-        cancelable = cjdata[keyvar]['2']
-        next_action = cjdata[keyvar]['3']
-        keyvar += 1
-        return utterance, cancelable, next_action
-                                              
+   def generation(self,cjdata,i):
+       fulldialogue = ''
+       dialogueA = 'DialogueActionTalkNoResponse('
+       dialogueU = 'utterance="' + cjdata[keyvar]['1']
+       dialogueC = '",cancelable="'
+       dialogueN = '",next_action='
+       try:
+           i+=1
+           # utterance  = u
+           # cancelable = c
+           # nextaction = n
+           keyvar = str(i)
+           fulldialogue += dialogueA
+           fulldialogue += dialogueU
+           fulldialogue += cjdata[keyvar]['1']
+           fulldialogue += dialogueC
+           fulldialogue += cjdata[keyvar]['2']
+           fulldialogue += dialogueN
+           fulldialogue += self.generation(cjdata,i)
+       except KeyError:
+           return "None"+ ')'
+       return fulldialogue
 
-        
+   #def funman(self):
+       #return exec(self.dialogue_string_test())
 
+   def funman2(self):
+       code = compile(dialogue_string_test, '<string>', 'exec')
+       exec (code)
 
-    def generation(self,cjdata,i):
-
-        fulldialogue = ''
-
-        dialogueA = 'DialogueActionTalkNoResponse('
-        dialogueU = 'utterance="' + cjdata[keyvar]['1']
-        dialogueC = '",cancelable="'
-        dialogueN = '",next_action='
-
-        try:
-            i+=1
-
-            # utterance  = u
-            # cancelable = c
-            # nextaction = n
-
-            keyvar = str(i)
-
-            fulldialogue += dialogueA
-            fulldialogue += dialogueU
-            fulldialogue += cjdata[keyvar]['1']
-            fulldialogue += dialogueC
-            fulldialogue += cjdata[keyvar]['2']
-            fulldialogue += dialogueN
-            fulldialogue += self.generation(cjdata,i)
-
-        except KeyError:
-            return "None"+ ')'
-
-        return fulldialogue
-
-    
-    #def funman(self):
-        #return exec(self.dialogue_string_test())
-    
-    def funman2(self):
-        code = compile(dialogue_string_test, '<string>', 'exec')
-        exec (code)
-        
-        """
+       """

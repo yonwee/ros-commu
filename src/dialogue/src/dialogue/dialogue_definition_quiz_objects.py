5efd0845c22c482da61881f288beb275275035e0
nemoninja <35476293+nemoninja@users.noreply.github.com>
2018-02-20 15:56:51 +0900

    Update dialogue_definition_quiz_objects.py

--- src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
+++ src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
@@ -31,10 +31,10 @@ class DialogueLibraryQuiz(DialogueLibrary):
 #         self.funman()
 #         f[4] = (DialogueActionTalkNoResponse(f[1], f[2], f[3]))
         f[4]=0
-        for x in range (4,0,-1):            
+        for x in range (3,0,-1):            
             f[0]=str(x)
             self.funman()
-            if x > 3:
+            if x > 2:
                 f[4] = DialogueActionTalkNoResponse(f[1], f[2], f[3])
             else:
                 f[4] = DialogueActionTalkNoResponse(f[1], f[2], f[4])
@@ -42,7 +42,7 @@ class DialogueLibraryQuiz(DialogueLibrary):
     
     
     def request_script(self):
-        convo = urllib2.urlopen('http://192.168.1.225:8080/?json={tedt}')
+        convo = urllib2.urlopen('http://192.168.1.225:8080/?json={test}')
         cjson = convo.read()
         cjdata = json.loads(cjson)
         #keyvar = "1"

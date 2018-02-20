38b08aed4ff1d1d79b93b719ee8f9a9f1041c81e
nemoninja <35476293+nemoninja@users.noreply.github.com>
2018-02-20 12:43:24 +0900

    Update dialogue_definition_quiz_objects.py

--- src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
+++ src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
@@ -31,11 +31,13 @@ class DialogueLibraryQuiz(DialogueLibrary):
         self.funman()
         #gitgud = (DialogueActionTalkNoResponse(f[1], f[2], f[3]))
         f[4] = (DialogueActionTalkNoResponse(f[1], f[2], f[3]))
-        while x > 0:
+        try:
             x=x-1
             f[0]=str(x)
             self.funman()
-            #f[4] = (DialogueActionTalkNoResponse(f[1], f[2], gitgud))
+            f[4] = (DialogueActionTalkNoResponse(f[1], f[2], gitgud))
+            except Keyerror:
+                pass
         return Dialogue(f[4])
     
     

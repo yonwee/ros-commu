a620e16e73d02040d7729732120b74c5d55bbd5f
nemoninja <35476293+nemoninja@users.noreply.github.com>
2018-02-20 14:55:10 +0900

    Update dialogue_definition_quiz_objects.py

--- src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
+++ src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
@@ -30,11 +30,11 @@ class DialogueLibraryQuiz(DialogueLibrary):
         f[0]=str(x)
         self.funman()
         f[4] = (DialogueActionTalkNoResponse(f[1], f[2], f[3]))
-        for y in range (-2,0):
-            x = y*-1
-            f[0]=str(x)
-            self.funman()
-            f[4] = DialogueActionTalkNoResponse(f[1], f[2], f[4])
+#         for y in range (-2,0):
+#             x = y*-1
+#             f[0]=str(x)
+#             self.funman()
+#             f[4] = DialogueActionTalkNoResponse(f[1], f[2], f[4])
         return Dialogue(f[4])
     
     

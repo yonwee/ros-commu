9cdbd0e9c8b9e4ac1754737c8719688aa2da5b66
nemoninja <35476293+nemoninja@users.noreply.github.com>
2018-02-20 12:31:13 +0900

    Update dialogue_definition_quiz_objects.py

--- src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
+++ src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
@@ -29,12 +29,12 @@ class DialogueLibraryQuiz(DialogueLibrary):
         x = 3
         f[0]=str(x)
         self.funman()
-        gitgud = (DialogueActionTalkNoResponse(f[1], f[2],f[3]))
-        while x > 0:
-            x-=1
-            f[0]=str(x)
-            self.funman()
-            gitgud = (DialogueActionTalkNoResponse(f[1], f[2], gitgud))
+        gitgud = (DialogueActionTalkNoResponse(f[1], f[2], f[3]))
+#         while x > 0:
+#             x-=1
+#             f[0]=str(x)
+#             self.funman()
+#             gitgud = (DialogueActionTalkNoResponse(f[1], f[2], gitgud))
         return Dialogue(gitgud)
     
     

a68a092a41d7315f76db28dde7d629c96c8d7145
nemoninja <35476293+nemoninja@users.noreply.github.com>
2018-02-22 12:57:53 +0900

    Update dialogue_definition_quiz_objects.py

--- src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
+++ src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
@@ -1,21 +1,22 @@
 import random
+import rospy
 
 from dialogue import Dialogue
 from dialogue_action import *
 from dialogue_manager import DialogueLibrary
+from dialogue_dictionary import convos
 
 import urllib2
 import json
-
 
 class DialogueLibraryQuiz(DialogueLibrary):
     """
     A DialogueLibrary that can be used when a CommU robot sees an object. This plays 'object hide-and-seek' with the user.
     """
 
-    global f, utterance, cancelable, next_action
-    
-    f = {}
+    global utterance, cancelable, next_action, store
+    store = {}
+
 
     def get_dialogue_for_topic(self, topic):
         # type: (str) -> Dialogue
@@ -24,42 +25,35 @@ class DialogueLibraryQuiz(DialogueLibrary):
         :param topic:   The label assigned by the ssd network
         :return:        The Dialogue concerning the object.
         """
-        #self.request_script()
-        f[5] = self.request_script()
-        for x in range (3,0,-1):            
-            f[0]=str(x)
-            self.funman()
-            if x > 2:
-                f[4] =  DialogueActionSleep(
-                    sleep_time=1,
-                    cancelable=False,
-                    next_action=DialogueActionTalkNoResponse(f[1].format(self.__get_object_noun(topic)), f[2], f[3]))                                                   
-            else:
-                f[4] = DialogueActionTalkNoResponse(f[1].format(self.__get_object_noun(topic)), f[2], f[4])
-        return Dialogue(f[4])
-        
+        #return Dialogue(DialogueActionTalkNoResponse(utterance='heya', cancelable=False, next_action=None))
+
+        cjdata = self.request_script()
+        store['full'] = cjdata
+        x=1
+        store['block'] = self.assign_return_dia(x)
+        return Dialogue(store['block'])
+
     def request_script(self):
-        convo = urllib2.urlopen('http://192.168.1.225:8080/?json={test}')
+        convo = urllib2.urlopen('http://192.168.1.166:9000/?json={test}')
         cjson = convo.read()
         cjdata = json.loads(cjson)
+        #cjdata = convos[1]
         return cjdata
-        
-    def funman(self):
-        #cjdata = self.request_script()
-        utterance = f[5][f[0]]['1']
-        cancelable = f[5][f[0]]['2']
-        next_action = None
-        #next_action = f[5][f[0]]['3'].replace("zz","None")
-        f[1] = utterance
-        f[2] = cancelable
-        f[3] = next_action
 
-    def __get_object_noun(self, label):
-        return self.object_proper_name_map.get(label, label)
+    def assign_return_dia(self,x):
+        curint = str(x)
+        if store['full'][curint]['type']=='last':
+            return DialogueActionTalkNoResponse(store['full'][curint]['u'],store['full'][curint]['c'],next_action=None)
+        if store['full'][curint]['type']=='pass':
+            next = int(store['full'][curint]['next'])
+            return DialogueActionTalkNoResponse(store['full'][curint]['u'],store['full'][curint]['c'],next_action=self.assign_return_dia(next)) #(x+1) moves interation up by one
+        if store['full'][curint]['type']=='binary':
+            yesloc = int(store['full'][curint]['yesloc'])
+            noloc  = int(store['full'][curint]['noloc'])
+            return DialogueActionTalkBinaryResponse(store['full'][curint]['u'],store['full'][curint]['c'],next_action_yes=self.assign_return_dia(yesloc),next_action_no=self.assign_return_dia(noloc))
 
-    object_proper_name_map = {
-        'aeroplane': 'airplane',
-        'diningtable': 'dining table',
-        'pottedplant': 'potted plant',
-        'tvmonitor': 'screen'
-    }
+    def return_none_bby(self):
+        return None
+
+    def return_arb_dia(self):
+        return DialogueActionTalkNoResponse(utterance='yes',cancelable=False,next_action=self.return_none_bby())

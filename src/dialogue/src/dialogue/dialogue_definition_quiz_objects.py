cc995465c09e25e39f7b686c6e68801935164c96
nemoninja <35476293+nemoninja@users.noreply.github.com>
2018-02-27 15:24:13 +0900

    manager.say_eng

--- src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
+++ src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
@@ -38,12 +38,13 @@ class DialogueLibraryQuiz(DialogueLibrary):
         return Dialogue(DialogueActionTalkNoResponse(
             utterance='hey',
             cancelable=False,
-            next_action=DialogueActionTalkBanzaiResponse(
-                gesture_file='banzai',
-                # gesture_file=open('gestures/banzai.s3r'),
-                cancelable=False,
-                next_action=None
-            )))
+            next_action=None
+            # next_action=DialogueActionTalkBanzaiResponse(
+            #     gesture_file='banzai',
+            #     # gesture_file=open('gestures/banzai.s3r'),
+            #     cancelable=False,
+            #     next_action=None)
+            ))
 
     def request_script(self):
         convo = urllib2.urlopen('http://192.168.1.166:9000/?json={test}')

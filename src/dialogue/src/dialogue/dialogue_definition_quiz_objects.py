b0a24d0257aeaa14a749c390e69d5aac84306121
nemoninja <35476293+nemoninja@users.noreply.github.com>
2018-02-28 11:17:04 +0900

    Trying just gu

--- src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
+++ src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
@@ -31,26 +31,30 @@ class DialogueLibraryQuiz(DialogueLibrary):
         # store['full'] = cjdata
         # store['block'] = self.assign_return_dia(topic,x=1)
         # return Dialogue(store['block'])
-        return Dialogue(DialogueActionTalkNoResponse(
-            'arm down', False, next_action=DialogueActionMove(
-            'acchi_arm_down', False, next_action=DialogueActionTalkNoResponse(
-                    'arm left', False, next_action=DialogueActionMove(
-                    'acchi_arm_left', False, next_action=DialogueActionTalkNoResponse(
-                            'arm right', False, next_action=DialogueActionMove(
-                            'acchi_arm_right', False, next_action=DialogueActionTalkNoResponse(
-                                    'arm up', False, next_action=DialogueActionMove(
-                                    'acchi_arm_up', False, next_action=DialogueActionTalkNoResponse(
-                                            'atama kaki', False, next_action=DialogueActionMove(
-                                            'atama_kaki', False, next_action=DialogueActionTalkNoResponse(
-                                                    'atama tonton', False, next_action=DialogueActionMove(
-                                                    'atama_tonton', False, next_action=DialogueActionTalkNoResponse(
-                                                            'choki', False, next_action=DialogueActionMove(
-                                                            'choki', False, next_action=DialogueActionTalkNoResponse(
-                                                                    'eyeblink', False, next_action=DialogueActionMove(
-                                                                    'eyeblink', False, next_action=DialogueActionTalkNoResponse(
-                                                                            'gu', False, next_action=DialogueActionMove(
-                                                                            'gu', False, next_action=None)))))))))))))))))))
 
+        # return Dialogue(DialogueActionTalkNoResponse(
+        #     'arm down', False, next_action=DialogueActionMove(
+        #     'acchi_arm_down', False, next_action=DialogueActionTalkNoResponse(
+        #             'arm left', False, next_action=DialogueActionMove(
+        #             'acchi_arm_left', False, next_action=DialogueActionTalkNoResponse(
+        #                     'arm right', False, next_action=DialogueActionMove(
+        #                     'acchi_arm_right', False, next_action=DialogueActionTalkNoResponse(
+        #                             'arm up', False, next_action=DialogueActionMove(
+        #                             'acchi_arm_up', False, next_action=DialogueActionTalkNoResponse(
+        #                                     'atama kaki', False, next_action=DialogueActionMove(
+        #                                     'atama_kaki', False, next_action=DialogueActionTalkNoResponse(
+        #                                             'atama tonton', False, next_action=DialogueActionMove(
+        #                                             'atama_tonton', False, next_action=DialogueActionTalkNoResponse(
+        #                                                     'choki', False, next_action=DialogueActionMove(
+        #                                                     'choki', False, next_action=DialogueActionTalkNoResponse(
+        #                                                             'eyeblink', False, next_action=DialogueActionMove(
+        #                                                             'eyeblink', False, next_action=DialogueActionTalkNoResponse(
+        #                                                                     'gu', False, next_action=DialogueActionMove(
+        #                                                                     'gu', False, next_action=None)))))))))))))))))))
+
+        return ((DialogueActionTalkNoResponse(
+            'gu', False, next_action=DialogueActionMove(
+            'gu', False, next_action=None))))
 
     def request_script(self):
         convo = urllib2.urlopen('http://192.168.1.166:9000/?json={test}')

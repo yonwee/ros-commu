0dc50b0c28104fc433760b51b153750602b2a708
ossxwi <36293223+ossxwi@users.noreply.github.com>
2018-03-05 15:47:05 +0900

    Update dialogue_definition_quiz_objects.py

--- src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
+++ src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
@@ -46,17 +46,23 @@ class DialogueLibraryQuiz(DialogueLibrary):
         curint = str(x)
 
         if store['full'][curint]['type']=='last':
-            #store['full'][curint]['u'] =store['full'][curint]['u'].format(topic=(self.__get_object_noun(topic)), atopic =self.__add_a_to_noun(self.__get_object_noun(topic)))
-            #for future grammar processing of the topic - anoun to add 'a' or 'an' into the topic string
-            #consider using inflect library as well
-            store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
+            store['full'][curint]['u'] =store['full'][curint]['u'].format(topic=self.__get_object_noun(topic),
+                                                                          atopic=self.__add_a_to_noun(
+                                                                              self.__get_object_noun(topic)))
+            # consider using inflect library as well
+            # store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
             return DialogueActionTalkNoResponse(store['full'][curint]['u'],
                                                 store['full'][curint]['c'],
                                                 next_action=None)
+            # return DialogueActionMove("banzai",
+            #                                     store['full'][curint]['c'],
+            #                                     next_action=None)
 
         if store['full'][curint]['type']=='pass':
             next = int(store['full'][curint]['next'])
-            store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
+            store['full'][curint]['u'] = store['full'][curint]['u'].format(topic=self.__get_object_noun(topic),
+                                                                           atopic=self.__add_a_to_noun(
+                                                                               self.__get_object_noun(topic)))
             return DialogueActionTalkNoResponse(store['full'][curint]['u'],
                                                 store['full'][curint]['c'],
                                                 next_action=self.assign_return_dia(topicstr,next))
@@ -64,7 +70,9 @@ class DialogueLibraryQuiz(DialogueLibrary):
         if store['full'][curint]['type']=='binary':
             yesloc = int(store['full'][curint]['yesloc'])
             noloc  = int(store['full'][curint]['noloc'])
-            store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
+            store['full'][curint]['u'] = store['full'][curint]['u'].format(topic=self.__get_object_noun(topic),
+                                                                           atopic=self.__add_a_to_noun(
+                                                                               self.__get_object_noun(topic)))
             return DialogueActionTalkBinaryResponse(store['full'][curint]['u'],
                                                     store['full'][curint]['c'],
                                                     next_action_yes=self.assign_return_dia(topicstr,yesloc),
@@ -74,21 +82,31 @@ class DialogueLibraryQuiz(DialogueLibrary):
             yesloc = int(store['full'][curint]['yesloc'])
             noloc = int(store['full'][curint]['noloc'])
             neuloc = int(store['full'][curint]['neuloc'])
-            store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
+            store['full'][curint]['u'] = store['full'][curint]['u'].format(topic=self.__get_object_noun(topic),
+                                                                           atopic=self.__add_a_to_noun(
+                                                                               self.__get_object_noun(topic)))
             return DialogueActionTalkTernaryResponse(store['full'][curint]['u'],
                                                     store['full'][curint]['c'],
                                                     next_action_yes=self.assign_return_dia(topicstr, yesloc),
                                                     next_action_no=self.assign_return_dia(topicstr, noloc),
                                                     next_action_neutral=self.assign_return_dia(topicstr,neuloc))
-        # #unimplemented look command
-        # if store['full'][curint]['type'] == 'look':
-        #     next = int(store['full'][curint]['next'])
-        #     lookat = int(store['full'][curint]['looktarget'])
-        #     #lookat should be DialogueActionLook.LOOK_TYPE_WATCH_CONVERSATION_PARTNER or similar
+        #unimplemented look command
+        if store['full'][curint]['type'] == 'look':
+            next = int(store['full'][curint]['next'])
+            lookat = int(store['full'][curint]['looktarget'])
+            #lookat should be DialogueActionLook.LOOK_TYPE_WATCH_CONVERSATION_PARTNER or similar
+            store['full'][curint]['u'] = store['full'][curint]['u'].format(topic=self.__get_object_noun(topic),
+                                                                           atopic=self.__add_a_to_noun(
+                                                                               self.__get_object_noun(topic)))
+            return DialogueActionLook(lookat,
+                                      store['full'][curint]['c'],
+                                      next_action=self.assign_return_dia(topicstr, next))
+
+        # if store['full'][curint]['type']=='move':
         #     store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
-        #     return DialogueActionLook(lookat,
-        #                               store['full'][curint]['c'],
-        #                               next_action=self.assign_return_dia(topicstr, next))
+        #     return DialogueActionMove("banzai",
+        #                                         store['full'][curint]['c'],
+        #                                         next_action=None)
 
 
 

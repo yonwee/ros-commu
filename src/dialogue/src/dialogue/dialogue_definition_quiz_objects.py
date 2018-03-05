2540e301daa84c8008a9bff2ab633f8e4fdacba5
nemoninja <35476293+nemoninja@users.noreply.github.com>
2018-03-05 13:49:31 +0900

    bilibili's current state

--- src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
+++ src/dialogue/src/dialogue/dialogue_definition_quiz_objects.py
@@ -25,111 +25,61 @@ class DialogueLibraryQuiz(DialogueLibrary):
         :return:        The Dialogue concerning the object.
         """
         #return Dialogue(DialogueActionTalkNoResponse(utterance='heya', cancelable=False, next_action=None))
-
-        # cjdata = self.request_script()
-        # cjdatalen = len(cjdata)
-        # store['full'] = cjdata
-        # store['block'] = self.assign_return_dia(topic,x=1)
-        # return Dialogue(store['block'])
-
-        # return Dialogue(DialogueActionTalkNoResponse(
-        #     'arm down', False, next_action=DialogueActionMove(
-        #     'acchi_arm_down', False, next_action=DialogueActionSleep(
-        #     sleep_time=1, cancelable=False, next_action=DialogueActionTalkNoResponse(
-        #             'arm left', False, next_action=DialogueActionMove(
-        #             'acchi_arm_left', False, next_action=DialogueActionSleep(
-        #             sleep_time=1, cancelable=False, next_action=DialogueActionTalkNoResponse(
-        #                     'arm right', False, next_action=DialogueActionMove(
-        #                     'acchi_arm_right', False, next_action=DialogueActionSleep(
-        #                     sleep_time=1, cancelable=False, next_action=DialogueActionTalkNoResponse(
-        #                             'arm up', False, next_action=DialogueActionMove(
-        #                             'acchi_arm_up', False, next_action=DialogueActionSleep(
-        #                             sleep_time=1, cancelable=False, next_action=DialogueActionTalkNoResponse(
-        #                                     'atama kaki', False, next_action=DialogueActionMove(
-        #                                     'atama_kaki', False, next_action=DialogueActionSleep(
-        #                                     sleep_time=1, cancelable=False, next_action=DialogueActionTalkNoResponse(
-        #                                             'atama tonton', False, next_action=DialogueActionMove(
-        #                                             'atama_tonton', False, next_action=DialogueActionSleep(
-        #                                             sleep_time=1, cancelable=False, next_action=DialogueActionTalkNoResponse(
-        #                                                     'choki', False, next_action=DialogueActionMove(
-        #                                                     'choki', False, next_action=DialogueActionSleep(
-        #                                                     sleep_time=1, cancelable=False, next_action=DialogueActionTalkNoResponse(
-        #                                                             'eyeblink', False, next_action=DialogueActionMove(
-        #                                                             'eyeblink', False, next_action=DialogueActionSleep(
-        #                                                             sleep_time=1, cancelable=False, next_action=DialogueActionTalkNoResponse(
-        #                                                                     'gu', False, next_action=DialogueActionMove(
-        #                                                                     'gu', False, next_action=DialogueActionSleep(
-        #                                                                     sleep_time=1, cancelable=False, next_action=None))))))))))))))))))))))))))))
-#
-#         return Dialogue(DialogueActionTalkNoResponse(
-#             'banzai2', False, next_action=DialogueActionMovePlus(
-#             'banzai2', """0.0	P	0.0	100	2	-80	3	0	-1
-# 0.0	P	0.0	100	4	15	5	0	-1
-# 0.1 t""",
-#              False, next_action=DialogueActionMove(
-#                                 'banzai2', False, next_action=DialogueActionSleep(
-#                                     sleep_time=1, cancelable=False, next_action=None)))))
-
-        return Dialogue(DialogueActionTalkNoResponse(
-            'acchi_arm_left2.s3r', False, next_action=DialogueActionMove(
-                    'acchi_arm_left2.s3r', False, next_action=DialogueActionSleep(
-                        sleep_time=1, cancelable=False, next_action=None))))
-
-    def request_script(self):
-        convo = urllib2.urlopen('http://192.168.1.166:9000/?json={test}')
+        convo = urllib2.urlopen("http://192.168.1.171:8080/?json={" + topic + "}")
         cjson = convo.read()
         cjdata = json.loads(cjson)
         rospy.loginfo("Received data from conversation server.")
-        return cjdata
+        cjdatalen = len(cjdata)
+        store['full'] = cjdata
+        store['block'] = self.assign_return_dia(topic,x=1)
+        return Dialogue(store['block'])
 
-    # def assign_return_dia(self,topic,x):
-    #     '''
-    #     Assigns the utterances and cancelable parameters of the respective Dialogue functions,
-    #     and reiterates for next action.
-    #     :param x:   The current conversation iteration point/number
-    #     :return:    Dialogue function
-    #     '''
-    #     topicstr = str(topic)
-    #     curint = str(x)
-    #
-    #     if store['full'][curint]['type']=='last':
-    #         #store['full'][curint]['u'] =store['full'][curint]['u'].format(topic=(self.__get_object_noun(topic)), atopic =self.__add_a_to_noun(self.__get_object_noun(topic)))
-    #         #for future grammar processing of the topic - anoun to add 'a' or 'an' into the topic string
-    #         #consider using inflect library as well
-    #         store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
-    #         # return DialogueActionTalkNoResponse(store['full'][curint]['u'],
-    #         #                                     store['full'][curint]['c'],
-    #         #                                     next_action=None)
-    #         return DialogueActionMove("banzai",
-    #                                             store['full'][curint]['c'],
-    #                                             next_action=None)
-    #
-    #     if store['full'][curint]['type']=='pass':
-    #         next = int(store['full'][curint]['next'])
-    #         store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
-    #         return DialogueActionMove("banzai",
-    #                                             store['full'][curint]['c'],
-    #                                             next_action=self.assign_return_dia(topicstr,next))
-    #
-    #     if store['full'][curint]['type']=='binary':
-    #         yesloc = int(store['full'][curint]['yesloc'])
-    #         noloc  = int(store['full'][curint]['noloc'])
-    #         store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
-    #         return DialogueActionTalkBinaryResponse(store['full'][curint]['u'],
-    #                                                 store['full'][curint]['c'],
-    #                                                 next_action_yes=self.assign_return_dia(topicstr,yesloc),
-    #                                                 next_action_no=self.assign_return_dia(topicstr,noloc))
-    #
-    #     if store['full'][curint]['type'] == 'ternary':
-    #         yesloc = int(store['full'][curint]['yesloc'])
-    #         noloc = int(store['full'][curint]['noloc'])
-    #         neuloc = int(store['full'][curint]['neuloc'])
-    #         store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
-    #         return DialogueActionTalkTernaryResponse(store['full'][curint]['u'],
-    #                                                 store['full'][curint]['c'],
-    #                                                 next_action_yes=self.assign_return_dia(topicstr, yesloc),
-    #                                                 next_action_no=self.assign_return_dia(topicstr, noloc),
-    #                                                 next_action_neutral=self.assign_return_dia(topicstr,neuloc))
+
+    def assign_return_dia(self,topic,x):
+        '''
+        Assigns the utterances and cancelable parameters of the respective Dialogue functions,
+        and reiterates for next action.
+        :param x:   The current conversation iteration point/number
+        :return:    Dialogue function
+        '''
+        topicstr = str(topic)
+        curint = str(x)
+
+        if store['full'][curint]['type']=='last':
+            #store['full'][curint]['u'] =store['full'][curint]['u'].format(topic=(self.__get_object_noun(topic)), atopic =self.__add_a_to_noun(self.__get_object_noun(topic)))
+            #for future grammar processing of the topic - anoun to add 'a' or 'an' into the topic string
+            #consider using inflect library as well
+            store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
+            return DialogueActionTalkNoResponse(store['full'][curint]['u'],
+                                                store['full'][curint]['c'],
+                                                next_action=None)
+
+        if store['full'][curint]['type']=='pass':
+            next = int(store['full'][curint]['next'])
+            store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
+            return DialogueActionTalkNoResponse(store['full'][curint]['u'],
+                                                store['full'][curint]['c'],
+                                                next_action=self.assign_return_dia(topicstr,next))
+
+        if store['full'][curint]['type']=='binary':
+            yesloc = int(store['full'][curint]['yesloc'])
+            noloc  = int(store['full'][curint]['noloc'])
+            store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
+            return DialogueActionTalkBinaryResponse(store['full'][curint]['u'],
+                                                    store['full'][curint]['c'],
+                                                    next_action_yes=self.assign_return_dia(topicstr,yesloc),
+                                                    next_action_no=self.assign_return_dia(topicstr,noloc))
+
+        if store['full'][curint]['type'] == 'ternary':
+            yesloc = int(store['full'][curint]['yesloc'])
+            noloc = int(store['full'][curint]['noloc'])
+            neuloc = int(store['full'][curint]['neuloc'])
+            store['full'][curint]['u'] = store['full'][curint]['u'].format(topicstr)
+            return DialogueActionTalkTernaryResponse(store['full'][curint]['u'],
+                                                    store['full'][curint]['c'],
+                                                    next_action_yes=self.assign_return_dia(topicstr, yesloc),
+                                                    next_action_no=self.assign_return_dia(topicstr, noloc),
+                                                    next_action_neutral=self.assign_return_dia(topicstr,neuloc))
         # #unimplemented look command
         # if store['full'][curint]['type'] == 'look':
         #     next = int(store['full'][curint]['next'])
